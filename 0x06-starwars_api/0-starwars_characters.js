797ae392dae5e79e510be81e62c44505cb86cf14
houssam980 <houssam.ghmari1243@gmail.com>
2024-07-11 15:20:40 +0000

    fixed style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,38 @@
 #!/usr/bin/node
-
+// Prints all characters
 
 if (process.argv.length !== 3) {
-    process.exit();
-  }
-  const movieID = process.argv[2];
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-  const request = require('request');
-  
+  process.exit();
+}
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+const request = require('request');
 
-  function printCharacter(urs) {
-    const characterURL = urs.shift();
-    if (characterURL) {
-      request(characterURL, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-          printCharacter(urs);
-        }
-      });
-    }
-  }
-  
-
-  function printWarsCharacters (url) {
-    request(url, function (error, response, body) {
+// Recursive function
+function printNextCharacter (urls) {
+  // Remove first URL
+  const characterURL = urls.shift();
+  if (characterURL) {
+    request(characterURL, function (error, response, body) {
       if (!error) {
-        printCharacter(JSON.parse(body).characters);
+        // Parse and print the character
+        console.log(JSON.parse(body).name);
+        // Move to next character
+        printNextCharacter(urls);
       }
     });
   }
-  
-  printWarsCharacters(url);
+}
+
+// Fetches a list
+function printStarWarsCharacters (url) {
+  // Get list of URL
+  request(url, function (error, response, body) {
+    if (!error) {
+      // Call recursive function
+      printNextCharacter(JSON.parse(body).characters);
+    }
+  });
+}
+
+printStarWarsCharacters(url);
